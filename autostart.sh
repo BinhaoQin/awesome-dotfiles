fb571c74f994449ccc0c18fcc1eef3367f6ed090
Binhao Qin <bhqin@umich.edu>
2022-05-13 20:07:12 -0400

    Update iwd and autostart

--- autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if (xrdb -query | grep -q "^awesome\\.started:\\s*true$") then
-  exit
-fi
-
-xrdb -merge <<< "awesome.started:true"
-dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"
